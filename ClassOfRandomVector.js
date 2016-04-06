95f4b40250f1149e1582bd0dc8de91f894156f7d
Ivan456 <ivan.vodich.l@gmail.com>
2016-04-06 19:17:30 +0300

    zoom out graphic. add function normal distribution density

--- ClassOfRandomVector.js
+++ ClassOfRandomVector.js
@@ -1,14 +1,14 @@
-//SRO_lab1_vodich
 	function ClassOfRandomVector() {
 		this.mu;
 		this.sigma;
+		this.Pi;
 		this.numberOfVectors;
 		this.color;
 		this.massiveOfXi = [];
 	};
 
 	ClassOfRandomVector.prototype.boxMullerCreateEta = function() {
-		var eta =[], 
+		var eta = [], 
 			alfa = [];
 		alfa[0] = Math.random();
 		alfa[1] = Math.random();
@@ -18,7 +18,7 @@
 	};
 
 	ClassOfRandomVector.prototype.decayHoleckogo = function() {
-		var a = [[],[]];
+		var a = [];
 		a[0][0] = this.sigma[0][0] / Math.sqrt(this.sigma[0][0]);
 		
 		a[1][0] = this.sigma[1][0] / Math.sqrt(this.sigma[0][0]);
@@ -74,12 +74,32 @@
  			console.log(this.massiveOfXi[i][0]);
  				var x = this.massiveOfXi[i][0],
  				y = this.massiveOfXi[i][1];
- 			ctx.fillRect(600 + x * 12 - 8, 300 + y * 12 - 4, 8, 8);
+ 			ctx.fillRect(600 + x * 4 - 2, 300 + y * 4 - 2, 4, 4);
  		};		
  		ctx.stroke();  
 	};
 
-	ClassOfRandomVector.prototype.normalDistribution = function() {
-
+	ClassOfRandomVector.prototype.normalDistributionDensity	= function(x) {	
+		var result =  1 / (2 * Math.PI);
 		
+		var determinatorSigma = this.sigma[0][0] * this.sigma[1][1]; 
+		determinatorSigma -= this.sigma[0][1] * this.sigma[1][0];
+		
+		result *= 1 / Math.sqrt(determinatorSigma);
+
+		var xMinusMu = [x[0] - mu[0], x[1] - mu[1]];
+		
+		var sigmaInverse = [];
+		sigmaInverse[0][0] = this.sigma[1][1] / determinatorSigma;
+		sigmaInverse[1][1] = this.sigma[0][0] / determinatorSigma;
+		sigmaInverse[0][1] = - this.sigma[1][0] / determinatorSigma;
+		sigmaInverse[1][0] = - this.sigma[0][1] / determinatorSigma;
+
+		var numerator = xMinusMu[0] * xMinusMu[0] * sigmaInverse[0][0];
+		numerator += xMinusMu[0] * xMinusMu[1] * sigmaInverse[1][0]; 
+		numerator += xMinusMu[0] * xMinusMu[1] * sigmaInverse[0][1]; 
+		numerator += xMinusMu[0] * xMinusMu[0] * sigmaInverse[1][1]; 
+		
+		result *= Math.exp(- numerator / 2);
+		return result;
 	};
--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-//SRO_lab1_vodich
 (function() {
 
 var L = 5,
@@ -15,22 +13,4 @@ for (var i = 0; i < L; i += 1) {
 	massiveOfObjectsOfClass[i].drawRandomVectors();	
 };
 
-=======
-//SRO_lab1_vodich
-(function() {
-
-var L = 5,
-	massiveOfObjectsOfClass = [];
-
-for (var i = 0; i < L; i += 1) {
-	massiveOfObjectsOfClass[i] = new ClassOfRandomVector();
-	massiveOfObjectsOfClass[i].color = Math.trunc(249 / L) * (i + 1);
-	massiveOfObjectsOfClass[i].mu = [30 * i - 20, 0 ];
-	massiveOfObjectsOfClass[i].sigma = [[20 + i * 3, 10 + i * 3], [10 + i * 3, 40 + i]];
-	massiveOfObjectsOfClass[i].numberOfVectors = 900;
-	massiveOfObjectsOfClass[i].createRandomVectors();
-	massiveOfObjectsOfClass[i].drawRandomVectors();	
-};
-
->>>>>>> f045c43cbfe27a4abb779b1764557dcd1da6691e
 })();
