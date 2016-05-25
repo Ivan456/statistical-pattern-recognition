0e341717f82c78d4cd4c6feef7cd5d5e6038aafb
Ivan456 <ivan.vodich.l@gmail.com>
2016-05-25 16:42:12 +0300

    add decision rule

--- ClassOfRandomVector.js
+++ ClassOfRandomVector.js
@@ -1,14 +1,18 @@
+var ClassOfRandomVector = (function () {
 	function ClassOfRandomVector() {
+		this.class;
+		this.massiveOfDecisionXi = [];
 		this.mu;
 		this.sigma;
 		this.Pi;
 		this.numberOfVectors;
 		this.color;
 		this.massiveOfXi = [];
+		this.zoom = 7;
 	};
 
 	ClassOfRandomVector.prototype.boxMullerCreateEta = function() {
-		var eta = [], 
+		var eta =[], 
 			alfa = [];
 		alfa[0] = Math.random();
 		alfa[1] = Math.random();
@@ -18,7 +22,7 @@
 	};
 
 	ClassOfRandomVector.prototype.decayHoleckogo = function() {
-		var a = [];
+		var a = [[],[]];
 		a[0][0] = this.sigma[0][0] / Math.sqrt(this.sigma[0][0]);
 		
 		a[1][0] = this.sigma[1][0] / Math.sqrt(this.sigma[0][0]);
@@ -71,12 +75,34 @@
 	ClassOfRandomVector.prototype.drawRandomVectors = function() {
         ctx.fillStyle = "rgb(100, " + (255 - this.color) + "," + this.color + ")";
 		for (var i = 0; i < this.numberOfVectors; i += 1) {
- 			console.log(this.massiveOfXi[i][0]);
- 				var x = this.massiveOfXi[i][0],
- 				y = this.massiveOfXi[i][1];
- 			ctx.fillRect(600 + x * 4 - 2, 300 + y * 4 - 2, 4, 4);
- 		};		
- 		ctx.stroke();  
+ 			var x = this.massiveOfXi[i][0],
+ 			y = this.massiveOfXi[i][1];
+ 			ctx.fillRect(600 + x * this.zoom  - 1, 300 - y * this.zoom - 1, 2, 2);
+ 		};	
+	};
+
+	ClassOfRandomVector.prototype.drawVectorsWithErrorClass = function() {
+		var x, y;
+		ctx.fillStyle = "red";
+		for (i = 0; i < this.numberOfVectors; i += 1) {
+ 			x = this.massiveOfXi[i][0],
+ 			y = this.massiveOfXi[i][1];
+ 			if (this.massiveOfDecisionXi[i] !== this.class) {		
+	 			ctx.fillRect(600 + x * this.zoom  - 1, 300 - y * this.zoom - 1, 2, 2);
+ 			};
+ 		};	
+	};
+
+	ClassOfRandomVector.prototype.drawVectorsWithProbality = function(p) {
+		var x, y;
+		ctx.fillStyle = "red";
+		for (i = 0; i < this.numberOfVectors; i += 1) {
+ 			x = this.massiveOfXi[i][0],
+ 			y = this.massiveOfXi[i][1];
+ 			if (this.normalDistributionDensity(this.massiveOfXi[i]) > p) {		
+	 			ctx.fillRect(600 + x * this.zoom  - 1, 300 - y * this.zoom - 1, 2, 2);
+ 			};
+ 		};	
 	};
 
 	ClassOfRandomVector.prototype.normalDistributionDensity	= function(x) {	
@@ -87,19 +113,47 @@
 		
 		result *= 1 / Math.sqrt(determinatorSigma);
 
-		var xMinusMu = [x[0] - mu[0], x[1] - mu[1]];
+		var xMinusMu = [x[0] - this.mu[0], x[1] - this.mu[1]];
 		
-		var sigmaInverse = [];
+		var sigmaInverse = [[], []];
 		sigmaInverse[0][0] = this.sigma[1][1] / determinatorSigma;
 		sigmaInverse[1][1] = this.sigma[0][0] / determinatorSigma;
-		sigmaInverse[0][1] = - this.sigma[1][0] / determinatorSigma;
-		sigmaInverse[1][0] = - this.sigma[0][1] / determinatorSigma;
+		sigmaInverse[0][1] = - this.sigma[0][1] / determinatorSigma;
+		sigmaInverse[1][0] = - this.sigma[1][0] / determinatorSigma;
 
 		var numerator = xMinusMu[0] * xMinusMu[0] * sigmaInverse[0][0];
 		numerator += xMinusMu[0] * xMinusMu[1] * sigmaInverse[1][0]; 
 		numerator += xMinusMu[0] * xMinusMu[1] * sigmaInverse[0][1]; 
-		numerator += xMinusMu[0] * xMinusMu[0] * sigmaInverse[1][1]; 
+		numerator += xMinusMu[1] * xMinusMu[1] * sigmaInverse[1][1]; 
 		
 		result *= Math.exp(- numerator / 2);
 		return result;
 	};
+
+	ClassOfRandomVector.prototype.recognition = function(massiveOfObjectsOfClass, w){
+		for (var i = 0; i < this.massiveOfXi.length; i++) {
+		 	this.massiveOfDecisionXi[i] = this.decisionRule(this.massiveOfXi[i], massiveOfObjectsOfClass, w);
+		};
+	};
+
+	ClassOfRandomVector.prototype.decisionRule = function(xi, massiveOfObjectsOfClass, w){
+		var f_i = [],
+	 		min_index_f_i = 0;
+		for (var i = 0; i < massiveOfObjectsOfClass.length; i++) {
+			f_i[i] = 0;
+			for (var j = 0; j < massiveOfObjectsOfClass.length; j++) {
+				f_i[i] += w[j][i] * massiveOfObjectsOfClass[j].Pi * massiveOfObjectsOfClass[j].normalDistributionDensity(xi);
+			};
+
+			if (f_i[i] < f_i[min_index_f_i]) {
+				min_index_f_i = i;
+			};
+		};
+
+		return min_index_f_i + 1;
+
+	};
+
+
+	return ClassOfRandomVector;
+})();
--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 (function() {
 
-var L = 5,
+	var L = 5,
 	massiveOfObjectsOfClass = [];
+	for (var i = 0; i < L; i += 1) {
+		massiveOfObjectsOfClass[i] = new ClassOfRandomVector();
+		massiveOfObjectsOfClass[i].class = i + 1;
+		massiveOfObjectsOfClass[i].Pi = 1/L;
+		massiveOfObjectsOfClass[i].color = Math.trunc(249 / L) * (i + 1);
+		massiveOfObjectsOfClass[i].mu = [30 * i - 20, 0 ];
+		massiveOfObjectsOfClass[i].sigma = [[20 + i * 3, 10 + i * 3], [10 + i * 3, 40 + i]];
+		massiveOfObjectsOfClass[i].numberOfVectors = 900;
+		massiveOfObjectsOfClass[i].createRandomVectors();
+		massiveOfObjectsOfClass[i].drawRandomVectors();	
+	};
 
-for (var i = 0; i < L; i += 1) {
-	massiveOfObjectsOfClass[i] = new ClassOfRandomVector();
-	massiveOfObjectsOfClass[i].color = Math.trunc(249 / L) * (i + 1);
-	massiveOfObjectsOfClass[i].mu = [30 * i - 20, 0 ];
-	massiveOfObjectsOfClass[i].sigma = [[20 + i * 3, 10 + i * 3], [10 + i * 3, 40 + i]];
-	massiveOfObjectsOfClass[i].numberOfVectors = 900;
-	massiveOfObjectsOfClass[i].createRandomVectors();
-	massiveOfObjectsOfClass[i].drawRandomVectors();	
-};
+	var w = [[0,1000000,1,1,1],[1,0,1,1,1],[1,1,0,1,1],[1,1,1,0,1],[1,1,1,1,0]];
+	for (var i = 0; i < L; i += 1) {
+		massiveOfObjectsOfClass[i].recognition(massiveOfObjectsOfClass,w);
+		massiveOfObjectsOfClass[i].drawVectorsWithErrorClass();	
+		//massiveOfObjectsOfClass[i].drawVectorsWithProbality(0.001);
+	};
 
 })();
